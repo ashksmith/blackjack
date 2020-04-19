290bb6145644eb6515bf9564cea431c4e01dd605
Ashley K. Smith <mail@ashleyksmith.com>
2020-04-19 10:46:20 +0300

    Updates

---
+++ precache-manifest.a0a87a0b8417a0c2c72c73b343b136c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618f94482b1550f0ec269c0dba227d98",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "075bcf0538e0e26cc9f4",
+    "url": "/blackjack/static/css/main.6928fd18.chunk.css"
+  },
+  {
+    "revision": "a576f858233a3f310bda",
+    "url": "/blackjack/static/js/2.88347340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blackjack/static/js/2.88347340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075bcf0538e0e26cc9f4",
+    "url": "/blackjack/static/js/main.668fec6c.chunk.js"
+  },
+  {
+    "revision": "b11b3d41b969f2374f54",
+    "url": "/blackjack/static/js/runtime-main.3b3fc7b5.js"
+  }
+]);
